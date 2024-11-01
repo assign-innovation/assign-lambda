274ae1c4b99e469aabff60704c64db42fac582b8
r_sato <r_sato@a-ssign.com>
2024-11-01 19:00:59 +0900

    FIX: first-meeting-reminder-click の更新

--- first-meeting-reminder-click/model/first-meeting-reminder-click.js
+++ first-meeting-reminder-click/model/first-meeting-reminder-click.js
@@ -1,61 +1,98 @@
-const squel = require("squel").useFlavour('mysql');
-const rds_connect = require('../src/rds_connect.js');
+import squel from "squel";
+import { data_api, begin, commit, rollback } from "../src/rds_connect.js";
 
-exports.index =  async function(event, rds){
-    
+squel.useFlavour("mysql");
+
+/**
+ * クリックイベント
+ * @param {*} event
+ * @param {*} rds
+ * @returns
+ */
+export const index = async (event, rds) => {
     const c_support_id = event.id;
-    if(Boolean(c_support_id) === false){
+
+    // c_support_id がない場合はエラーを返す
+    if (!Boolean(c_support_id)) {
         return {
             statusCode: 400,
             body: JSON.stringify({
-                res: "invalid id"
+                res: "c_support_id is required",
             }),
             headers: {
-                'Content-Type': 'application/json',
-            }
+                "Content-Type": "application/json",
+            },
         };
     }
-    
-    // クリックログをtrueにする
-    const update_sql = squel.update()
-                            .table("c_supports")
-                            .set("is_reminder_clicked", true)
-                            .where("id = ?", c_support_id)
-                            .toString();
-    
-    // 担当エージェントの名前、電話番号、メールアドレスを返す
-    const select_sql = squel.select()
-                            .from("c_supports")
-                            .join("agents", null, "c_supports.agent_id = agents.id")
-                            .field("CONCAT(agents.last_name, ' ', agents.first_name)", "agent_name")
-                            .field("agents.phone")
-                            .field("agents.email")
-                            .where("c_supports.id = ?", c_support_id)
-                            .toString();
 
-    // 結果を返却する
-    return await Promise.all([rds_connect.data_api("update", update_sql, rds), rds_connect.data_api("select", select_sql, rds)])
-        .then(result => {
-            console.log(result);
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    res: result[1]["select"][0]
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            };
-        })
-        .catch(err => {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({
-                    res: err
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            };
-        });
+    // --------------------------------
+    // クリックフラグを true にする
+    // --------------------------------
+    // SQL の生成
+    const update_sql = squel
+        .update()
+        .table("c_supports")
+        .set("is_reminder_clicked", true)
+        .where("id = ?", c_support_id)
+        .where("agent_id = ?", 156) // todo: けす
+        .toString();
+
+    // Data API にアクセス
+    const transactionId = await begin(); // トランザクションを開始
+    try {
+        await data_api(update_sql, transactionId);
+        await commit(transactionId);
+    } catch (error) {
+        console.error("Error during update:", error);
+        await rollback(transactionId);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                res: `is_reminder_clicked update error : ${error}`,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+    }
+
+    // --------------------------------
+    // 担当エージェントの名前、電話番号、メールアドレスを返す
+    // --------------------------------
+    // SQL の生成
+    const select_sql = squel
+        .select()
+        .from("c_supports")
+        .join("agents", null, "c_supports.agent_id = agents.id")
+        .field("CONCAT(agents.last_name, ' ', agents.first_name)", "agent_name")
+        .field("agents.phone")
+        .field("agents.email")
+        .where("c_supports.id = ?", c_support_id)
+        .where("c_supports.agent_id = ?", 156) // todo: けす
+        .toString();
+
+    // Data API にアクセス
+    try {
+        const select_result = await data_api(select_sql);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                res: select_result[0],
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                res: `first-meeting-reminder-click error : ${err}`,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+    }
 };
